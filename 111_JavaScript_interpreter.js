f2c07e02723a84e772bdb7fec0a82371dc63297f
LexiciScriptor <lexiciscriptor@libero.it>
2016-01-21 18:00:04 +0100

    Create 111_JavaScript_interpreter.js

---
+++ 111_JavaScript_interpreter.js
@@ -0,0 +1,39 @@
+//111 interpreter in JavaScript by Lexici Scriptor
+//111 by Ehird @ Esolang
+
+var s = ""; //insert here the 111 code; fuck no standard input
+var l = 0, e = 0; //ones, exit
+var p, r; //pattern, replacement
+
+function red(i){ //counts and removes initial 'i's
+    if ((s.length === 0)||(s[0] != i)) return 0;
+    s = s.slice(1);
+    return 1 + red(i);
+}
+
+function rep(s,p,r){ //replaces p with r in s
+    if ((s.length < p.length)) return s;
+    if (s.slice(0,p.length) == p) return r + rep(s.slice(p.length),p,r);
+    return s[0] + rep(s.slice(1),p,r);
+}
+
+do {
+    console.log(s);
+    l = red(1);
+    if (l === 0) e = -1;
+    else if (s.length < l + 1) e = 1;
+        else {
+            p = s.slice(1,l+1);
+            s = s.slice(l+1);
+            l = red(1);
+            if (s.length < l + 1) e = 1;
+            else {
+                r = s.slice(1,l+1);
+                s = s.slice(l+1);
+                s = rep(s,p,r);
+            }
+        }
+} while (e === 0);
+
+if (e === -1) console.log("Infinite loop: initial zero.");
+if (e === 1) console.log("End.");
